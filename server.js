f7ef1ab0411787af95579d737e33d42aaad90ea9
IaroslavLasiichuk <84236230+IaroslavLasiichuk@users.noreply.github.com>
2023-04-22 10:00:15 -0600

    Server created

---
+++ server.js
@@ -0,0 +1,51 @@
+// Require express - routes - controllers - session - helpers and - handlebars
+import path from 'path';
+import express from 'express';
+import session from 'express-session';
+// import routes from './server/controllers';
+
+// Const sequelize store.
+import sequelize from './server/config/connection.js';
+import SequelizeStoreConstructor from 'connect-session-sequelize';
+const SequelizeStore = SequelizeStoreConstructor(session.Store);
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// Gets the express app and port.
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Import sequelize connection
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 900000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Sets up the session.
+app.use(session(sess));
+
+// Use express static json and urlencoded.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Applies routes to the application.
+// app.use(routes);
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// Turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  console.log('Hello');
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT}`));
+  });
---
+++ server/db/schema.sql
@@ -0,0 +1,4 @@
+-- Crates database
+DROP DATABASE IF EXISTS portfolio_db;
+CREATE DATABASE  portfolio_db;
+USE portfolio_db;
